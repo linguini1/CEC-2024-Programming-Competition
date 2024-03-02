9386dcf7039c3034ebd8323ed2592b17ee7ac7bb
Matteo Golin <matteo.golin@gmail.com>
2024-03-02 14:31:07 -0500

    Resource maps are now normalized and can be averaged.

--- logic/resource.py
+++ logic/resource.py
@@ -1,4 +1,6 @@
 import csv
+import glob
+import re
 from dataclasses import dataclass
 from enum import StrEnum
 from typing import TypeAlias, Optional
@@ -33,6 +35,27 @@ class Resource:
 ResourceMap: TypeAlias = list[list[Optional[Resource]]]  # Defines the resource map as a grid of resources
 
 
+def normalize_resources(resources: ResourceMap) -> None:
+    """
+    Normalizes all resource values in the resource map to have a minimum value of 0.
+    Args:
+        resources: The resource to normalize.
+    """
+
+    # Get the minimum resource value
+    minimum = float("inf")
+    for row in resources:
+        for tile in row:
+            if tile is not None and tile.value != float("-inf") and tile.value < minimum:
+                minimum = tile.value
+
+    # Add the absolute value of the minimum resource to bring all resource values to >= 0
+    for row in resources:
+        for tile in row:
+            if tile is not None:
+                tile.value += abs(minimum)
+
+
 def load_resource(filepath: str, rtype: ResourceType) -> ResourceMap:
     """
     Loads resource data from a CSV file into a resource map.
@@ -64,10 +87,11 @@ def load_resource(filepath: str, rtype: ResourceType) -> ResourceMap:
 
             map[y][x] = Resource(rtype, value)
 
+    normalize_resources(map)
     return map
 
 
-def mask_resource(resources: ResourceMap, world: World) -> None:
+def mask_land_resources(resources: ResourceMap, world: World) -> None:
     """
     Remove any resources from the resource map that fall on land tiles, as they cannot be moved to. This will modify the
     original resource map.
@@ -80,3 +104,64 @@ def mask_resource(resources: ResourceMap, world: World) -> None:
         for y in range(WORLD_HEIGHT):
             if world[y][x] == WorldTile.LAND:
                 resources[y][x] = None
+
+
+def mask_preserved_tiles(resources: ResourceMap, preserve: ResourceMap) -> None:
+    """
+    Reduce the perceived value of resource tiles that coincide with a preserved resource.
+    Args:
+        resources: The resource map to reduce values of.
+        preserve: The location of all resources that should be preserved.
+    """
+
+    for x in range(WORLD_WIDTH):
+        for y in range(WORLD_HEIGHT):
+            if preserve[y][x] is not None and resources[y][x] is not None:
+                resources[y][x].value -= preserve[y][x].value  # type: ignore
+
+
+def load_all_resources(glob_path: str, rtype: ResourceType) -> list[ResourceMap]:
+    """
+    Loads all of the resource files for a particular resource and returns them in order of ascending days.
+    Args:
+        glob_path: A wildcard path that will collect all of the resource files required.
+        rtype: The resource type being loaded
+    """
+    files = []
+    for file in glob.glob(glob_path):
+        number = int(re.search(r"[0-9]+", file).group())  # type: ignore
+        files.append((file, number))
+
+    resources = []
+    for file, _ in sorted(files, key=lambda x: x[1]):
+        oil = load_resource(file, rtype)
+        resources.append(oil)
+
+    return resources
+
+
+def average_resource_values(resource_series: list[ResourceMap], rtype: ResourceType) -> ResourceMap:
+    """
+    Averages the resource values across the entire map.
+    Args:
+        resource_series: A list of resource maps to average.
+        rtype: The resource type being averaged.
+    Returns:
+        A resource map containing the average resource values for each tile.
+    """
+    average_resource: ResourceMap = [[None for _ in range(WORLD_WIDTH)] for _ in range(WORLD_HEIGHT)]
+    for x in range(WORLD_WIDTH):
+        for y in range(WORLD_HEIGHT):
+
+            # Compute the average of a single tile across the entire list
+            sum = 0
+            for resources in resource_series:
+                if resources[y][x] is not None:
+                    sum += resources[y][x].value  # type: ignore
+
+            if sum == 0:  # There are no resources at all (likely land)
+                average_resource[y][x] = None
+            else:
+                average_resource[y][x] = Resource(rtype, sum / len(resource_series))
+
+    return average_resource
--- test.py
+++ test.py
@@ -1,21 +1,53 @@
-from logic.resource import load_resource, ResourceType, ResourceMap
+import math
+from logic.resource import (
+    average_resource_values,
+    load_all_resources,
+    load_resource,
+    ResourceType,
+    ResourceMap,
+    mask_land_resources,
+    mask_preserved_tiles,
+)
 from logic.world import WORLD_WIDTH, WORLD_HEIGHT, WorldTile, load_world
 
-result_map: ResourceMap = [[None for _ in range(100)] for _ in range(100)]
 
-world = load_world("./data/world_array_data_day_1.csv")
-oil = load_resource("./data/oil_data_day_1.csv", ResourceType.OIL)
+def print_heatmap(resource: ResourceMap):
+    heat_map = ".:+@#"
+    maximum = float("-inf")
+    minimum = float("inf")
+    for row in resource:
+        for tile in row:
+            if tile is not None:
+                if tile.value > maximum:
+                    maximum = tile.value
+                if tile.value < minimum and tile.value != float("-inf"):
+                    minimum = tile.value
 
-# Mask out any land oil
-for y in range(WORLD_HEIGHT):
-    for x in range(WORLD_WIDTH):
-        if world[x][y] == WorldTile.OCEAN:
-            result_map[y][x] = oil[y][x]
+    for row in resource:
+        for tile in row:
+            if tile is None:
+                print(" ", end="")
+            else:
+                normalized = (tile.value - minimum) / (maximum - minimum) * (len(heat_map) - 1 - 0) + 0
+                if normalized == float("inf"):
+                    i = len(heat_map) - 1
+                elif normalized == float("-inf"):
+                    i = 0
+                else:
+                    i = math.floor(normalized)
+                print(heat_map[i], end="")
+        print()
 
-for row in world:
-    for tile in row:
-        if tile == WorldTile.LAND:
-            print(".", end="")
-        else:
-            print("x", end="")
-    print()
+
+world = load_world("./data/world_array_data_day_1.csv")  # World is constant
+
+oils = load_all_resources("./data/oil*", ResourceType.OIL)
+corals = load_all_resources("./data/coral*", ResourceType.CORAL)
+
+for oil in oils:
+    mask_land_resources(oil, world)
+
+for oil, coral in zip(oils, corals):
+    mask_preserved_tiles(oil, coral)
+
+average_oil = average_resource_values(oils, ResourceType.OIL)
